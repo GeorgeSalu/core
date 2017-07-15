1acb009637889898b5eaf0228d91b15f9dcf3a08
George Salu <george.salu10@gmail.com>
2017-07-15 20:59:49 -0300

    Java 8 Sum: Array, Map and List Collection Example using reduce() and collect() Method

---
+++ core-java/src/br/com/java8/stream/dto/Line.java
@@ -0,0 +1,14 @@
+package br.com.java8.stream.dto;
+
+public class Line {
+	
+	private int length;
+	
+	public Line(int length) {
+		this.length = length;
+	}
+        public int getLength() {
+		return length;
+	}
+	
+}
