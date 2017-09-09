1896a60175afd899c3deb157929762bcb01cf9fc
George Salu <george.salu10@gmail.com>
2017-09-09 11:08:14 -0300

    try-with-resources for AutoCloseable Class in Java

---
+++ core-java/src/br/com/java7/core/TryWithResources.java
@@ -0,0 +1,21 @@
+package br.com.java7.core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+public class TryWithResources {
+	public static void main(String[] args) throws Exception {
+
+		File file = new File("D:/cp/jar/testjar.jar");
+		JarEntry je;
+
+		try (FileInputStream fis = new FileInputStream(file); JarInputStream jis = new JarInputStream(fis);) {
+			while ((je = jis.getNextJarEntry()) != null) {
+				System.out.println(je.getName());
+			}
+		}
+
+	}
+}
