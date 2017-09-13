af276b9d769d69a3e817453a9f0980387cac4c55
George Salu <george.salu10@gmail.com>
2017-09-12 21:04:34 -0300

    Traverse a Directory Structure Using Files.walkFileTree in Java NIO 2

---
+++ core-java/src/br/com/java7/core/WalkFileTreeCopyExample.java
@@ -0,0 +1,40 @@
+package br.com.java7.core;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
+
+public class WalkFileTreeCopyExample {
+	public static void main(String[] args) throws IOException {
+		final Path sourceDir = Paths.get("D:/cp/source");
+		final Path targetDir = Paths.get("D:/cp/dest");
+		Files.walkFileTree(sourceDir, EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE,
+				new SimpleFileVisitor<Path>() {
+					@Override
+					public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+						Path target = targetDir.resolve(sourceDir.relativize(dir));
+						try {
+							Files.copy(dir, target);
+						} catch (FileAlreadyExistsException e) {
+							if (!Files.isDirectory(target))
+								throw e;
+						}
+						return FileVisitResult.CONTINUE;
+					}
+
+					@Override
+					public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+						Files.copy(file, targetDir.resolve(sourceDir.relativize(file)));
+						return FileVisitResult.CONTINUE;
+					}
+				});
+
+	}
+}
