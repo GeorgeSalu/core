ffefd4c9711b928c8467e090df86465d87f92ce1
George Salu <george.salu10@gmail.com>
2017-10-05 20:22:58 -0300

    Encode and Decode in Java NIO

---
+++ core-java/src/br/com/java6/core2/EncodeDecodeExample.java
@@ -0,0 +1,25 @@
+package br.com.java6.core2;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
+public class EncodeDecodeExample {
+	public static void main(String[] args) throws CharacterCodingException {
+		Charset charset = Charset.forName("UTF-8");
+		CharsetDecoder chdecoder = charset.newDecoder();
+		CharsetEncoder chencoder = charset.newEncoder();
+		String s = "Encode and Decode Example.";
+		ByteBuffer byteBuffer = ByteBuffer.wrap(s.getBytes());
+		CharBuffer charBuffer = chdecoder.decode(byteBuffer);
+		ByteBuffer newByteBuff = chencoder.encode(charBuffer);
+		while (newByteBuff.hasRemaining()) {
+			char ch = (char) newByteBuff.get();
+			System.out.print(ch);
+		}
+		newByteBuff.clear();
+	}
+}
