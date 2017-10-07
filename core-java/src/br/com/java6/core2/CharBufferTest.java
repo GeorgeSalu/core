170d05b519f474be60068a82e53bbc68b1d0430c
George Salu <george.salu10@gmail.com>
2017-10-06 23:04:08 -0300

    Example of CharBuffer in Java

---
+++ core-java/src/br/com/java6/core2/CharBufferTest.java
@@ -0,0 +1,28 @@
+package br.com.java6.core2;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.CharBuffer;
+
+public class CharBufferTest {
+	public static void main(String[] args) {
+		try {
+			Reader rd = new BufferedReader(new InputStreamReader(new FileInputStream("file.txt")));
+			CharBuffer chbuff = CharBuffer.allocate(1024);
+			while (rd.read(chbuff) > 0) {
+				chbuff.flip();
+				while (chbuff.hasRemaining()) {
+					char ch = chbuff.get();
+					System.out.print(ch);
+				}
+				chbuff.clear();
+			}
+			rd.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
