895f2ce430e51f8400f7fce72ba1838e49ba1c06
George Salu <george.salu10@gmail.com>
2017-10-29 16:22:48 -0200

    Example of ReadWriteLock and ReentrantReadWriteLock in Java

---
+++ core-java/src/br/com/java6/core/Main.java
@@ -0,0 +1,15 @@
+package br.com.java6.core;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+	public static void main(String[] args) {
+		final int threadCount = 2;
+		final ExecutorService exService = Executors.newFixedThreadPool(threadCount);
+		final ScoreBoard scoreBoard = new ScoreBoard();
+		exService.execute(new ScoreUpdateThread(scoreBoard));
+		exService.execute(new ScoreHealthThread(scoreBoard));
+		exService.shutdown();
+	}
+}
---
+++ core-java/src/br/com/java6/core/ScoreHealthThread.java
@@ -0,0 +1,21 @@
+package br.com.java6.core;
+
+public class ScoreHealthThread implements Runnable {
+	private ScoreBoard scoreBoard;
+
+	public ScoreHealthThread(ScoreBoard scoreTable) {
+		this.scoreBoard = scoreTable;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Match Health: " + scoreBoard.getMatchHealth());
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}	
